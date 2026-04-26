fed59ad60b20c1950c79cd82a8783f9452bc4552
zhaodongpo <zhaodongpopopo@gmail.com>
2026-04-26 22:20:18 +0800

    handle mysql

--- client/src/main/java/com/maozhua/mz/AppApplication.java
+++ client/src/main/java/com/maozhua/mz/AppApplication.java
@@ -1,8 +1,12 @@
 package com.maozhua.mz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@MapperScan(basePackages = "com.maozhua.mz.infra.mapper")
 @SpringBootApplication(scanBasePackages = "com.maozhua.mz")
 public class AppApplication {
 
--- infra/src/main/java/com/maozhua/mz/infra/mysqlConfig/MyBatisMysqlConfig.java
+++
@@ -1,9 +0,0 @@
-package com.maozhua.mz.infra.mysqlConfig;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "com.maozhua.mz.infra.mapper")
-public class MyBatisMysqlConfig {
-}
